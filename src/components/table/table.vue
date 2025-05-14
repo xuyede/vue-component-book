c1dfc5fdedce68def4689421927a9f629e83721f
xuyede <yd.xu@wisetimes.cn>
2025-05-14 09:40:32 +0800

    feat: table

---
+++ src/components/table/table.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="col in columns">{{ col.title }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in data">
+        <td v-for="col in columns">{{ row[col.key] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts" setup name="ITable">
+import { ref } from 'vue'
+
+const props = defineProps({
+  columns: {
+    type: Array<any>,
+    default () {
+      return [];
+    }
+  },
+  data: {
+    type: Array<any>,
+    default () {
+      return [];
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+table{
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  empty-cells: show;
+  border: 1px solid #e9e9e9;
+}
+table th{
+  background: #f7f7f7;
+  color: #5c6b77;
+  font-weight: 600;
+  white-space: nowrap;
+}
+table td, table th{
+  padding: 8px 16px;
+  border: 1px solid #e9e9e9;
+  text-align: left;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueSetupExtend()],
+  plugins: [vue(), vueJsx(), vueSetupExtend()],
   resolve: {
     alias: {
       'vue': 'vue/dist/vue.esm-bundler.js'
